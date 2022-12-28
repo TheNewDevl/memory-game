eeaffe58d6c65ff2ad7a704cf887ccf6b5197028
carlos <carl.devfs@gmail.com>
2022-12-28 23:29:06 +0100

    add reset history

--- src/UI/Views/LobbyView/LobbyView.tsx
+++ src/UI/Views/LobbyView/LobbyView.tsx
@@ -4,12 +4,20 @@ import {Button} from "../../Components/Button/Button";
 import {Title} from "../../Components/Title/Title";
 import {useGameContext} from "../../../GameContext/GameContext";
 import {GameStateEnum} from "../../../types";
+import {setLocalStorage} from "../../../functions/localStorage";
 
 interface LobbyProps {
 }
 
 export const LobbyView = ({}: LobbyProps) => {
-  const {setGameState, gameHistory} = useGameContext()
+  const {setGameState, gameHistory, setGameHistory} = useGameContext()
+
+  const resetGameHistory = () => {
+    const gameHistory = {victories: 0, defeats: 0}
+    setGameHistory(gameHistory)
+    setLocalStorage('gameHistory', gameHistory)
+  }
+
   return (
     <div className={style.Lobby}>
       <Title text={'Memory Game'} />
@@ -18,6 +26,7 @@ export const LobbyView = ({}: LobbyProps) => {
         <p>Defeats : <span className={style.victories}>{gameHistory?.defeats && gameHistory?.defeats}</span></p>
       </div>
       <Button text={'Jouer'} onClick={()=>setGameState(GameStateEnum.PLAY)}/>
+      <Button text={'Reset Stats'} onClick={resetGameHistory}/>
     </div>
   );
 };
